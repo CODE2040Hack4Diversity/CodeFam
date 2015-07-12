9c86ccb4283edafe028b48840464052584db8ee8
glopes <gui.carneiro15@gmail.com>
2015-07-11 17:21:33 -0700

    deleted side menu and create new ew icon on top menu

--- www/app/controllers/main.js
+++ www/app/controllers/main.js
@@ -3,9 +3,11 @@
   var persons = [
   { name: "Guilherme Carneiro",
     user: "@glopes",
+    liked: false,
   },
   { name: "Something",
     user: "@something",
+    liked: false,
   },
 
   ];
@@ -13,12 +15,24 @@
 
   app.controller('MainCtrlIndex', function($scope, $ionicPopover){
     $scope.clicked = false;
+    $scope.filter = false;
     $scope.index = 0;
     $scope.persons = persons;
 
     $scope.see_more = function(){
       $scope.clicked = !$scope.clicked;
     };
+
+    $scope.see_filter = function(){
+      $scope.filter = !$scope.filter;
+    };
+
+    $scope.like = function(person){
+      person.liked = true;
+      $scope.clicked = false;
+      $scope.index = $scope.index + 1;
+    };
+
 
     $scope.get_next = function(){
       $scope.clicked = false;
